4ec54ae9d7678469c7bc2ad211b29ad486587b8c
Ya Feng <fengya04@126.com>
2017-09-03 21:52:16 +0800

    markdown

--- src/components/X.js
+++ src/components/X.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import markdownIt from 'markdown-it'
 
 import { randomPlaces, replace } from '../util'
 
 import './X.css'
+
+const md = markdownIt({
+  html: true,
+  breaks: true
+})
 
 export default ({ content }) => {
   const matches = content.match(/[\w\d]+/gi)
@@ -14,17 +20,17 @@ export default ({ content }) => {
       <Row>
         <Col xs='12' sm='4'
           dangerouslySetInnerHTML={{
-            __html: content
+            __html: md.renderInline(content)
           }}
         />
         <Col xs='12' sm='4'
           dangerouslySetInnerHTML={{
-            __html: replace(content, places)
+            __html: md.renderInline(replace(content, places))
           }}
         />
         <Col xs='12' sm='4'
           dangerouslySetInnerHTML={{
-            __html: replace(content, places, true)
+            __html: md.renderInline(replace(content, places, true))
           }}
         />
       </Row>
